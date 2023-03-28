2c503cbe52bb6f784365650e64c976d44cdfa63b
RafaelLyra8 <rafaellyra2010@hotmail.com>
2023-03-28 10:46:41 -0300

    Code improvements

---
+++ README.md
@@ -0,0 +1,1 @@
+There isn't any description to this library yet!
---
+++ dist/cjs/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MyCounter = void 0;
+var tslib_1 = require("tslib");
+var MyCounter_1 = tslib_1.__importDefault(require("./components/MyCounter"));
+exports.MyCounter = MyCounter_1.default;
+//# sourceMappingURL=index.js.map
