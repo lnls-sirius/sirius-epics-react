10ec707a285c914471b34e9816b088cc50d96680
Rafael Lyra <rafael.lyra@lnls.br>
2023-04-17 13:21:43 -0300

    Test file fix

--- src/tests/GeneralTest.test.tsx
+++ src/tests/GeneralTest.test.tsx
@@ -4,8 +4,8 @@ import { act, fireEvent, render, screen } from '@testing-library/react';
 import SiriusTooltip from '../components/SiriusTooltip';
 
 describe('Sirius Tooltip', () => {
-  it("Component renders", () => {
-    act(() => {
+  it("Component renders", async () => {
+    await act(async () => {
       render(
         <SiriusTooltip text="Test tooltip">
           This is a test
@@ -17,16 +17,18 @@ describe('Sirius Tooltip', () => {
     expect(element).toBeInTheDocument();
   })
 
-  it('Hover is working', () => {
+  it('Hover is working', async () => {
     const hover_txt: string = "Test tooltip";
 
-    render(
-      <SiriusTooltip text={hover_txt}>
-        <div aria-label="element">Element</div>
-      </SiriusTooltip>
-    )
+    await act(async () => {
+      render(
+        <SiriusTooltip text={hover_txt}>
+          <div aria-label="element">Element</div>
+        </SiriusTooltip>
+      );
+    });
 
-    fireEvent.click(screen.getByText(/Element/i), {button: 1})
+    fireEvent.click(screen.getByText(/Element/i), {button: 1});
     expect(screen.getByText(hover_txt)).toBeInTheDocument()
   })
 })
